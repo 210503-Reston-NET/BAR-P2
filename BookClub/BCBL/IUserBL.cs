b2514bd19963d0e9187cbed41110c1899bbb8010
lateu <richardlateu@gmail.com>
2021-06-07 19:02:58 -0400

    implementing user logique and test using swagger

---
+++ BookClub/BCBL/IUserBL.cs
@@ -0,0 +1,16 @@
+﻿
+using System.Collections.Generic;
+using BCModel;
+
+
+namespace BCBL
+{
+  public  interface IUserBL
+    {
+        public User AddUser(User u);
+        public User UpdateUser(User u);
+        public User GetUserByEmail(string email);
+
+        public List<User> GetAllUsers();
+    }
+}
---
+++ BookClub/BCWebUI/Controllers/UserController.cs
@@ -0,0 +1,56 @@
+﻿using BCBL;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BCModel;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BCWebUI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserBL _userBL;
+        // GET: api/<CustomerController>
+        public UserController(IUserBL e) { this._userBL = e; }
+        // GET: api/<UserController>
+        [HttpGet]
+        public IActionResult GetAllUsers()
+        {
+            return Ok(_userBL.GetAllUsers().ToList());
+        }
+
+        // GET api/<UserController>/5
+        [HttpGet("{email}")]
+        public User GetUserByEmail(string email)
+        {
+            return _userBL.GetUserByEmail(email);
+        }
+
+        // POST api/<UserController>
+        [HttpPost]
+        public void AddUser([FromBody] User user)
+        {
+            _userBL.AddUser(user);
+        }
+
+        // PUT api/<UserController>/5
+        [HttpPut("{email}")]
+        public void UpdateUser(string email, [FromBody] User user)
+        {
+            _userBL.UpdateUser(user);
+        }
+
+        // DELETE api/<UserController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
