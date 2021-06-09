331ae023cda4e4a8b06b07d859fd6cce8157474f
Bryce Zimbelman <bryce.zimbelman@snhu.edu>
2021-06-09 09:44:33 -0500

    Made Swagger work when published

--- BookClub/BCWebUI/Startup.cs
+++ BookClub/BCWebUI/Startup.cs
@@ -68,6 +68,14 @@ namespace BCWebUI
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "BCWebUI v1"));
             }
 
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "BCWebUI v1");
+                c.RoutePrefix = string.Empty;
+            });
+
             app.UseHttpsRedirection();
 
             app.UseRouting();
