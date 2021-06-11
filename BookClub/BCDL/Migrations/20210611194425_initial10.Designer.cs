db1189986893ae3c8ed69649588b49caf87160b2
AmanuelBoru <amgararo@gmail.com>
2021-06-11 15:46:46 -0400

    added pages read to user table

--- BookClub/BCDL/Migrations/20210611194425_initial10.Designer.cs
+++ BookClub/BCDL/Migrations/20210611194425_initial10.Designer.cs
@@ -2,15 +2,17 @@
 using BCDL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace BCDL.Migrations
 {
     [DbContext(typeof(BookClubDBContext))]
-    partial class BookClubDBContextModelSnapshot : ModelSnapshot
+    [Migration("20210611194425_initial10")]
+    partial class initial10
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
